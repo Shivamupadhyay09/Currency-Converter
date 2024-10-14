0d7b4b0f1435f876b711fd98640bcfe50df2200f
Shivamupadhyay09 <shivamupadhyay9451@gmail.com>
2024-10-14 10:49:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,4 +38,5 @@ def index():
     return render_template('index.html', currencies=currencies, conversion_result=conversion_result, error_message=error_message)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable or default to 5000
+    app.run(host='0.0.0.0', port=port
